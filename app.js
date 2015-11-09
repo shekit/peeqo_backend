b9b299d0a066df1e121c23469ec67ce9e68c4d5e
shekit <as7790@nyu.edu>
2015-11-08 21:40:01 -0500

    included body parser

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var routes = require('./routes/index')
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 
 app.use('/', routes)
